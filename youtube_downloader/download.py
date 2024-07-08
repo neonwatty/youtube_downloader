addcc57c4d89442f2630f28f1ad221bcd4173900
Jeremy Watt <jermwatt@gmail.com>
2024-07-08 11:16:35 -0700

    Update download.py

--- youtube_downloader/download.py
+++ youtube_downloader/download.py
@@ -2,11 +2,13 @@ from pytube import YouTube
 from moviepy.editor import VideoFileClip, AudioFileClip
 import requests
 import tempfile
+import re
 
 
 def download_joint_stream(yt: YouTube, itag: int, save_dir: str, yt_title: str) -> str:
     try:
-        final_save_path = save_dir + "/" + yt_title + ".mp4"   
+        final_save_path = save_dir + "/" + yt_title + ".mp4"
+        final_save_path = re.sub(r'[^a-zA-Z0-9./]', ' ', final_save_path)
         yt.streams.get_by_itag(itag).download(filename=final_save_path)
         return final_save_path
     except Exception as e:
@@ -18,7 +20,11 @@ def download_separate_streams_and_join(
 ) -> str:
     with tempfile.TemporaryDirectory() as tmpdirname:
         tmpaudiopath = tmpdirname + "/" + yt_title + "_audio.mp4"
+        tmpaudiopath = re.sub(r'[^a-zA-Z0-9./]', ' ', tmpaudiopath)
+
         tmpvideopath = tmpdirname + "/" + yt_title + "_video.mp4"
+        tmpvideopath = re.sub(r'[^a-zA-Z0-9./]', ' ', tmpvideopath)
+
         yt.streams.get_by_itag(audio_itag).download(filename=tmpaudiopath)
         yt.streams.get_by_itag(video_itag).download(filename=tmpvideopath)
 
@@ -27,6 +33,7 @@ def download_separate_streams_and_join(
         audio_clip = AudioFileClip(tmpaudiopath)
         video_clip.audio = audio_clip
         final_save_path = save_dir + "/" + yt_title + ".mp4"
+        final_save_path = re.sub(r'[^a-zA-Z0-9./]', ' ', final_save_path)
         video_clip.write_videofile(
             final_save_path,
             codec="libx264",
